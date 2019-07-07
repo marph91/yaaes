04ed7ccf87503f0e0c8f3d89619ba7a3f088cf3e
Martin Dörfelt <martin.d@andix.de>
2019-07-07 19:48:51 +0200

    shortened key expansion stages

--- src/key_expansion.vhd
+++ src/key_expansion.vhd
@@ -18,15 +18,15 @@ entity key_exp is
 end entity key_exp;
 
 architecture rtl of key_exp is
-  signal slv_stage : std_logic_vector(1 to 4) := (others => '0');
+  signal slv_stage : std_logic_vector(1 to 2) := (others => '0');
   signal a_sub_word,
-         a_rot_word,
-         a_rcon_word,
          a_rcon : t_word := (others => (others => '0'));
   signal a_data_out : t_state := (others => (others => (others => '0')));
 begin
   process(isl_clk)
-  variable v_data_out : t_state;
+    variable v_data_out : t_state;
+    variable v_rot_word,
+             v_rcon_word : t_word;
   begin
     if rising_edge(isl_clk) then
       slv_stage <= (isl_valid or isl_next_key) & slv_stage(slv_stage'LOW to slv_stage'HIGH-1);
@@ -37,34 +37,32 @@ begin
         a_data_out <= ia_data;
       end if;
 
-      -- rotate
+      -- rotate and substitute
       if slv_stage(1) = '1' then
-        for row in a_rot_word'RANGE loop
-          a_rot_word((row-1) mod 4) <= a_data_out(row, 3);
+        -- rotate
+        for row in ia_data'RANGE loop
+          v_rot_word((row-1) mod 4) := a_data_out(row, 3);
+        end loop;
+
+        -- substitute
+        for col in ia_data'RANGE loop
+          a_sub_word(col) <= C_SBOX(to_integer(v_rot_word(col)));
         end loop;
       end if;
 
-      -- substitute
+      -- xor rcon and last word
       if slv_stage(2) = '1' then
-        for col in a_rot_word'RANGE loop
-          a_sub_word(col) <= C_SBOX(to_integer(a_rot_word(col)));
-        end loop;
-      end if;
-
-      -- xor rcon
-      if slv_stage(3) = '1' then
-        for col in a_sub_word'RANGE loop
-          a_rcon_word(col) <= a_sub_word(col) xor a_rcon(col);
+        -- xor rcon
+        for col in ia_data'RANGE loop
+          v_rcon_word(col) := a_sub_word(col) xor a_rcon(col);
         end loop;
 
-        -- calculate round constant, as defined in: "FIPS 197, 5.2 Key Expansion"
+        -- calculate round constant for the next round, as defined in: "FIPS 197, 5.2 Key Expansion"
         a_rcon(0) <= double(a_rcon(0));
-      end if;
 
-      -- xor last word
-      if slv_stage(4) = '1' then
-        for row in a_rot_word'RANGE loop
-          v_data_out(row, 0) := a_data_out(row, 0) xor a_rcon_word(row);
+        -- xor last word
+        for row in ia_data'RANGE loop
+          v_data_out(row, 0) := a_data_out(row, 0) xor v_rcon_word(row);
           -- assign the following three words -> no sub, rot, ... needed
           v_data_out(row, 1) := v_data_out(row, 0) xor a_data_out(row, 1);
           v_data_out(row, 2) := v_data_out(row, 1) xor a_data_out(row, 2);
