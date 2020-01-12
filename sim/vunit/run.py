754a81cd6fd1a127fbebc1913e5795704f6d116f
Martin Doerfelt <martin.d@andix.de>
2020-01-12 17:18:32 +0100

    added code coverage if available

--- sim/vunit/run.py
+++ sim/vunit/run.py
@@ -5,6 +5,7 @@
 from glob import glob
 import importlib.util
 import os
+import subprocess
 
 from vunit import VUnit
 
@@ -18,6 +19,14 @@ def create_test_suites(prj):
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
         mod.create_test_suite(prj)
+
+    # add code coverage if gcc is available
+    ghdl_version = subprocess.check_output(["ghdl", "--version"]).decode()
+    if "GCC" in ghdl_version:
+        prj.set_sim_option("enable_coverage", True)
+        prj.set_compile_option(
+            "ghdl.flags", ["-g", "-fprofile-arcs", "-ftest-coverage"])
+        prj.set_sim_option("ghdl.elab_flags", ["-Wl,-lgcov", "-Wl,--coverage"])
 
 
 if __name__ == "__main__":
