c629bfa1ef96782053cbf2f5884da78ced9091b9
Martin Doerfelt <martin.d@andix.de>
2020-01-13 18:47:08 +0100

    moved the options to the previous position

--- sim/vunit/run.py
+++ sim/vunit/run.py
@@ -18,20 +18,19 @@ def create_test_suites(prj):
     lib.add_source_files(os.path.join(root, "vunit_common_pkg.vhd"))
     lib.add_source_files("../../src/*.vhd")
 
+    for run_script in run_scripts:
+        spec = importlib.util.spec_from_file_location("run", run_script)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        mod.create_test_suite(prj)
+
     # add code coverage if gcc is available
-    # add the options early to only analyze source files
     ghdl_version = subprocess.check_output(["ghdl", "--version"]).decode()
     if "GCC" in ghdl_version:
         prj.set_sim_option("enable_coverage", True)
         prj.set_compile_option(
             "ghdl.flags", ["-g", "-fprofile-arcs", "-ftest-coverage"])
         prj.set_sim_option("ghdl.elab_flags", ["-Wl,-lgcov", "-Wl,--coverage"])
-
-    for run_script in run_scripts:
-        spec = importlib.util.spec_from_file_location("run", run_script)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        mod.create_test_suite(prj)
 
 
 if __name__ == "__main__":
