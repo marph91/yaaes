ca174ac4d26f1dce1332eec2de85d73894813f2c
Martin Doerfelt <martin.d@andix.de>
2020-02-10 19:54:12 +0100

    updated the synthesis script

--- syn/synth.sh
+++ syn/synth.sh
@@ -8,14 +8,14 @@ rm -rf build
 mkdir -p build
 cd build
 
-ghdl -a --std=08 "$ROOT"/src/aes_pkg.vhd
-ghdl -a --std=08 "$ROOT"/src/input_conversion.vhd
-ghdl -a --std=08 "$ROOT"/src/output_conversion.vhd
-ghdl -a --std=08 "$ROOT"/src/key_expansion.vhd
-ghdl -a --std=08 "$ROOT"/src/cipher.vhd
-ghdl -a --std=08 "$ROOT"/src/aes.vhd
-# ghdl --synth --std=08 cipher
-yosys -m ghdl -p 'ghdl --std=08 aes; synth_ice40 -json aes.json'
+ghdl -a --std=08 --work=aes_lib "$ROOT"/src/aes_pkg.vhd
+ghdl -a --std=08 --work=aes_lib "$ROOT"/src/input_conversion.vhd
+ghdl -a --std=08 --work=aes_lib "$ROOT"/src/output_conversion.vhd
+ghdl -a --std=08 --work=aes_lib "$ROOT"/src/key_expansion.vhd
+ghdl -a --std=08 --work=aes_lib "$ROOT"/src/cipher.vhd
+ghdl -a --std=08 --work=aes_lib "$ROOT"/src/aes.vhd
+# ghdl --synth --std=08 --work=aes_lib cipher
+yosys -m ghdl -p 'ghdl --std=08  --work=aes_lib aes; synth_ice40 -json aes.json'
 # nextpnr-ice40 --hx1k --json aes.json --pcf ../constraints/ir.pcf --asc aes.asc --pcf-allow-unconstrained
 # icepack aes.asc aes.bin
 # iceprog aes.bin
