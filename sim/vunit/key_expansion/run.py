adc1cdc3692c08562d90536254b63c66f0c27675
Martin Dörfelt <martin.d@andix.de>
2020-03-22 11:15:54 +0100

    added key expansion for aes-256
    - Fixed bug where columns and rows were switched (see testbench).
    - Parametrized length of the keys.
    - Added short key expansion routine.

--- sim/vunit/key_expansion/run.py
+++ sim/vunit/key_expansion/run.py
@@ -15,7 +15,10 @@ def create_test_suite(ui):
     lib.add_source_files(os.path.join(root, "src", "*.vhd"))
 
     tb_key_expansion = lib.entity("tb_key_expansion")
-    tb_key_expansion.add_config(name="reference_vector")
+
+    for key_bits in [128, 256]:
+        tb_key_expansion.add_config(name=f"reference_vector_aes-{key_bits}",
+                                    generics={"C_KEY_WORDS": int(key_bits/32)})
 
 
 if __name__ == "__main__":
