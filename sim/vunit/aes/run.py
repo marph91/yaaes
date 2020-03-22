8bfca356ddaa1bc38dd7b586e1f347eac59f703b
Martin Dörfelt <martin.d@andix.de>
2020-03-22 19:19:39 +0100

    refactored aes and aes selftest
    - Removed duplicate code.
    - Removed for loop chaining.
    - Added an aes-256 selftest.

--- sim/vunit/aes/run.py
+++ sim/vunit/aes/run.py
@@ -4,6 +4,7 @@
 
 
 from binascii import a2b_hex
+import itertools
 import os
 
 from Cryptodome.Cipher import AES
@@ -71,76 +72,75 @@ def create_test_suite(ui):
 
     tb_aes = lib.entity("tb_aes")
 
+    # test configs
+    # TODO: duplicated at aes selftest
+    # TODO: allow more variability, e. g. varying segment_size for CFB
+    cfg1 = {
+        "input": "same",
+        "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
+        "C_PLAINTEXT2": "3243f6a8885a308d313198a2e0370734",
+        "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
+    }
+    cfg2 = {
+        "input": "different",
+        "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
+        "C_PLAINTEXT2": "000102030405060708090a0b0c0d0e0f",
+        "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
+    }
+    cfg3 = {
+        "input": "random",
+        "C_PLAINTEXT1": common.random_hex(32),
+        "C_PLAINTEXT2": common.random_hex(32),
+        "C_KEY": common.random_hex(32),
+    }
+    cfg4 = {
+        "input": "same",
+        "C_PLAINTEXT1": "00112233445566778899aabbccddeeff",
+        "C_PLAINTEXT2": "00112233445566778899aabbccddeeff",
+        "C_KEY": "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+    }
+
     # simulate two rounds of en- and decrypting for each chaining mode
     # TODO: implement counter mode. this would require some more input signals
-    for encryption in [0, 1]:
+    test_params = itertools.product((0, 1), ("ECB", "CBC", "CFB", "OFB"),
+                                    (cfg1, cfg2, cfg3, cfg4))
+    for encryption, mode, gen in test_params:
+        if not encryption and mode in ["ECB", "CBC"]:
+            continue  # not yet implemented
+
+        # TODO: python byteorder is LSB...MSB, VHDL is MSB downto LSB
         encr_str = "encrypt" if encryption else "decrypt"
         encr_func = encrypt if encryption else decrypt
-        for mode in ["ECB", "CBC", "CFB", "OFB"]:
-            if not encryption and mode in ["ECB", "CBC"]:
-                continue  # not yet implemented
-            # TODO: allow more variability, e. g. varying segment_size for CFB
-            gen1 = {
-                "input": "same",
-                "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
-                "C_PLAINTEXT2": "3243f6a8885a308d313198a2e0370734",
-                "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
-                "C_IV": common.random_hex(32),
-                "C_ENCRYPTION": encryption,
-                }
-            gen2 = {
-                "input": "different",
-                "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
-                "C_PLAINTEXT2": "000102030405060708090a0b0c0d0e0f",
-                "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
-                "C_IV": common.random_hex(32),
-                "C_ENCRYPTION": encryption,
-                }
-            gen3 = {
-                "input": "random",
-                "C_PLAINTEXT1": common.random_hex(32),
-                "C_PLAINTEXT2": common.random_hex(32),
-                "C_KEY": common.random_hex(32),
-                "C_IV": common.random_hex(32),
-                "C_ENCRYPTION": encryption,
-                }
-            gen4 = {
-                "input": "same",
-                "C_PLAINTEXT1": "00112233445566778899aabbccddeeff",
-                "C_PLAINTEXT2": "00112233445566778899aabbccddeeff",
-                "C_KEY": "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
-                "C_IV": common.random_hex(32),
-                "C_ENCRYPTION": encryption,
-                }
+        bw = 128
+        bw_key = len(gen["C_KEY"]) * 4  # 2 hex chars -> 8 bits
+        init_vector = common.random_hex(32)
 
-            bw = 128
-            for gen in [gen1, gen2, gen3, gen4]:
-                # TODO: python byteorder is LSB...MSB, VHDL is MSB downto LSB
-                bw_key = len(gen["C_KEY"]) * 4  # 2 hex chars -> 8 bits
-                ciphertext1, iv2 = encr_func(
-                    gen["C_PLAINTEXT1"], gen["C_KEY"], gen["C_IV"], mode)
-                ciphertext2, _ = encr_func(
-                    gen["C_PLAINTEXT2"], gen["C_KEY"], iv2, mode)
-                gen.update({
-                    "C_BITWIDTH_IF": bw,
-                    "C_BITWIDTH_KEY": bw_key,
-                    "C_MODE": mode,
-                    "C_CIPHERTEXT1": ciphertext1,
-                    "C_CIPHERTEXT2": ciphertext2,
-                    })
-                tb_aes.add_config(
-                    name="aes_%d_%s_mode_%s_bw_%d_input_%s" % (
-                        bw_key, encr_str, mode, bw, gen.pop("input")),
-                    generics=gen)
+        ciphertext1, iv2 = encr_func(
+            gen["C_PLAINTEXT1"], gen["C_KEY"], init_vector, mode)
+        ciphertext2, _ = encr_func(
+            gen["C_PLAINTEXT2"], gen["C_KEY"], iv2, mode)
+        gen.update({
+            "C_ENCRYPTION": encryption,
+            "C_BITWIDTH_IF": bw,
+            "C_BITWIDTH_KEY": bw_key,
+            "C_MODE": mode,
+            "C_CIPHERTEXT1": ciphertext1,
+            "C_CIPHERTEXT2": ciphertext2,
+            "C_IV": init_vector,
+        })
+        generics = {k: v for k, v in gen.items() if k != "input"}
+        tb_aes.add_config(
+            name="aes_%d_%s_mode_%s_bw_%d_input_%s" % (
+                bw_key, encr_str, mode, bw, gen["input"]),
+            generics=generics)
 
+        if gen["input"] == "random":
             # Add test for 8 and 32 bit bitwidth.
-            # Use stimuli and references from gen3.
-            for bw in [8, 32]:
-                gen3.update({"C_BITWIDTH_IF": bw})
-                bw_key = len(gen3["C_KEY"]) * 4  # 2 hex chars -> 8 bits
+            # Use stimuli and references from updated gen3.
+            for bw in (8, 32):
                 tb_aes.add_config(name="aes_%d_%s_mode=%s_bw=%d_input=random"
                                   % (bw_key, encr_str, mode, bw),
-                                  generics=gen3)
+                                  generics=generics)
 
 
 if __name__ == "__main__":
--- sim/vunit/aes_selftest/run.py
+++ sim/vunit/aes_selftest/run.py
@@ -3,6 +3,7 @@
 """Do selftests for the AES VHDL design."""
 
 
+import itertools
 import os
 
 from vunit import VUnit
@@ -18,49 +19,48 @@ def create_test_suite(ui):
     lib.add_source_files(os.path.join(root, "src", "*.vhd"))
     tb_aes = lib.entity("tb_aes_selftest")
 
+    # test configs
+    cfg1 = {
+        "input": "same",
+        "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
+        "C_PLAINTEXT2": "3243f6a8885a308d313198a2e0370734",
+        "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
+    }
+    cfg2 = {
+        "input": "different",
+        "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
+        "C_PLAINTEXT2": "000102030405060708090a0b0c0d0e0f",
+        "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
+    }
+    cfg3 = {
+        "input": "random",
+        "C_PLAINTEXT1": common.random_hex(32),
+        "C_PLAINTEXT2": common.random_hex(32),
+        "C_KEY": common.random_hex(32),
+    }
+    cfg4 = {
+        "input": "same",
+        "C_PLAINTEXT1": "00112233445566778899aabbccddeeff",
+        "C_PLAINTEXT2": "00112233445566778899aabbccddeeff",
+        "C_KEY": "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+    }
+
     # simulate two rounds of en- and decrypting for each chaining mode
-    bw_key = 128
-    for mode in ["CFB", "OFB"]:
-        # TODO: allow more variability, e. g. varying segment_size for CFB
-        gen1 = {
-            "input": "same",
-            "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
-            "C_PLAINTEXT2": "3243f6a8885a308d313198a2e0370734",
-            "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
-            "C_IV": common.random_hex(32),
-            "C_BITWIDTH_KEY": bw_key,
-            }
-        gen2 = {
-            "input": "different",
-            "C_PLAINTEXT1": "3243f6a8885a308d313198a2e0370734",
-            "C_PLAINTEXT2": "000102030405060708090a0b0c0d0e0f",
-            "C_KEY": "2b7e151628aed2a6abf7158809cf4f3c",
-            "C_IV": common.random_hex(32),
-            "C_BITWIDTH_KEY": bw_key,
-            }
-        gen3 = {
-            "input": "random",
-            "C_PLAINTEXT1": common.random_hex(32),
-            "C_PLAINTEXT2": common.random_hex(32),
-            "C_KEY": common.random_hex(32),
-            "C_IV": common.random_hex(32),
-            "C_BITWIDTH_KEY": bw_key,
-            }
-
+    test_params = itertools.product(("CFB", "OFB"), (cfg1, cfg2, cfg3, cfg4))
+    for mode, gen in test_params:
         bw = 128
-        for gen in [gen1, gen2, gen3]:
-            gen.update({"C_BITWIDTH_IF": bw,
-                        "C_MODE": mode})
-            tb_aes.add_config(
-                name="mode=%s_bw=%d_input=%s" % (mode, bw, gen.pop("input")),
-                generics=gen)
+        bw_key = len(gen["C_KEY"]) * 4  # 2 hex chars -> 8 bits
 
-        # Add test for 8 and 32 bit bitwidth.
-        # Use stimuli and references from gen3.
-        for bw in [8, 32]:
-            gen3.update({"C_BITWIDTH_IF": bw})
-            tb_aes.add_config(name="mode=%s_bw=%d_input=random"
-                              % (mode, bw), generics=gen3)
+        gen.update({
+            "C_BITWIDTH_IF": bw,
+            "C_BITWIDTH_KEY": bw_key,
+            "C_MODE": mode,
+            "C_IV": common.random_hex(32),
+        })
+        tb_aes.add_config(
+            name="aes_%d_mode_%s_bw_%d_input_%s" % (
+                bw_key, mode, bw, gen["input"]),
+            generics={k: v for k, v in gen.items() if k != "input"})
 
 
 if __name__ == "__main__":
